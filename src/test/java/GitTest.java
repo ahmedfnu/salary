0dafe4b2b765f5825cb8e77f1ecfd3b30fb8ab4f
ahmedfnu <s700ns@gmail.com>
2021-03-16 18:50:53 -0500

    salary update1

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -2,5 +2,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("100_000_00$");
+        System.out.println("10");
+        System.out.println("200");
     }
 }
