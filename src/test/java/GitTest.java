7c7ce1c3ca6aba71d5c3df78e469c6bba0914ae3
ahmedfnu <s700ns@gmail.com>
2021-03-16 18:55:08 -0500

    salary update1111

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
         System.out.println("100_000_00$");
         System.out.println("10");
         System.out.println("200");
-        System.out.println("erkan");
+        System.out.println("new day ");
     }
 }
